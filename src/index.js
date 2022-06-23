49696985fbe58b3c5f695be158daeecf2cdab91c
sArianV <62109635+sArianV@users.noreply.github.com>
2022-06-23 10:26:01 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const { app } = require('./app.js');
 
 async function startServer() {
     try {
-        app.listen(4000);
-        console.log('Server corriendo en puerto 4000');
+        app.listen(process.env.PORT || 4000);
+        console.log('Server corriendo en puerto'+ process.env.PORT );
     } catch (error) {
         console.log(error);
     }
